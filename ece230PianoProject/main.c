b522e5dd0c80e9223e380ea31db9f85415b3b6de
rutantj <rutantj@137.112.226.227>
2021-02-14 18:23:39 -0500

    Removed unnecessary UART code

--- ece230PianoProject/main.c
+++ ece230PianoProject/main.c
@@ -8,28 +8,6 @@
 
 #include "speaker.h"
 
-//![Simple UART Config]
-/* UART Configuration Parameter. These are the configuration parameters to
- * make the eUSCI A UART module to operate with a 9600 baud rate. These
- * values were calculated using the online calculator that TI provides
- * at:
- *http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSP430BaudRateConverter/index.html
- */
-const eUSCI_UART_ConfigV1 uartConfig =
-{
-        EUSCI_A_UART_CLOCKSOURCE_SMCLK,          // SMCLK Clock Source
-        13,                                     // BRDIV = 78
-        0,                                       // UCxBRF = 2
-        37,                                       // UCxBRS = 0
-        EUSCI_A_UART_ODD_PARITY,                  // No Parity
-        EUSCI_A_UART_LSB_FIRST,                  // LSB First
-        EUSCI_A_UART_ONE_STOP_BIT,               // One stop bit
-        EUSCI_A_UART_MODE,                       // UART mode
-        EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION,  // Oversampling
-        EUSCI_A_UART_8_BIT_LEN                  // 8 bit data length
-};
-//![Simple UART Config]
-
 int main(void)
 {
     /* Halting WDT  */
@@ -39,43 +17,11 @@ int main(void)
 
     SpeakerBasicFunction();
 
-    /* Selecting P1.2 and P1.3 in UART mode */
-    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
-            GPIO_PIN2 | GPIO_PIN3, GPIO_PRIMARY_MODULE_FUNCTION);
-
-    /* Setting DCO to 12MHz */
-    CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_12);
-
-    //![Simple UART Example]
-    /* Configuring UART Module */
-    MAP_UART_initModule(EUSCI_A0_BASE, &uartConfig);
-
-    /* Enable UART module */
-    MAP_UART_enableModule(EUSCI_A0_BASE);
-
-    /* Enabling interrupts */
-    MAP_UART_enableInterrupt(EUSCI_A0_BASE, EUSCI_A_UART_RECEIVE_INTERRUPT);
-    MAP_Interrupt_enableInterrupt(INT_EUSCIA0);
     MAP_Interrupt_enableSleepOnIsrExit();
     MAP_Interrupt_enableMaster();   
-    //![Simple UART Example]
 
     while(1)
     {
         MAP_PCM_gotoLPM0();
     }
 }
-
-/* EUSCI A0 UART ISR - Echoes data back to PC host */
-void EUSCIA0_IRQHandler(void)
-{
-    uint32_t status = MAP_UART_getEnabledInterruptStatus(EUSCI_A0_BASE);
-
-    if(status & EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
-    {
-        MAP_UART_transmitData(EUSCI_A0_BASE, MAP_UART_receiveData(EUSCI_A0_BASE));
-    }
-
-}
-
-// Test comment for T.J.'s push
